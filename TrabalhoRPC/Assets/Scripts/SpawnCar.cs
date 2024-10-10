2666a33fc0f68f5dfe3a4f7bb8c515cfade102ae
Jaspiooooon <joao14antuness@gmail.com>
2024-10-10 10:11:29 -0300

    metodo start e GetRandomSpawnPosition

--- TrabalhoRPC/Assets/Scripts/SpawnCar.cs
+++ TrabalhoRPC/Assets/Scripts/SpawnCar.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun; // Importa Photon para funcionalidades de multiplayer.
 
-public class SpawnCar : MonoBehaviour
+public class MultiplayerSetup : MonoBehaviourPunCallbacks // Define a classe para gerenciar o setup do multiplayer.
 {
-    // Start is called before the first frame update
+    public GameObject carPrefab; // Referência ao prefab do carro que será instanciado.
+
     void Start()
     {
-        
+        if (PhotonNetwork.IsConnected) // Verifica se o jogador está conectado à rede Photon.
+        {
+            PhotonNetwork.Instantiate(carPrefab.name, GetRandomSpawnPosition(), Quaternion.identity);
+            // Instancia o carro para o jogador com uma posição aleatória e rotação padrão.
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Função que retorna uma posição aleatória para spawnar o carro.
+    Vector3 GetRandomSpawnPosition()
     {
-        
+        return new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)); // Retorna uma posição aleatória no eixo X e Z.
     }
 }
